179ff94d5289d6d4f4230483f69d616ab247ea93
Alexey Lagun <alexey.lagun@gmail.com>
2022-05-02 16:00:40 +0300

    feat: add solution mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function minesweeper( matrix ) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  const resArr = [];
+	for (let i = 0; i < matrix.length; i++) {
+	  resArr[i] = matrix[i].slice();
+	}
+	for (let i = 0; i < resArr.length; i++) {
+ 	  for (let j = 0; j < resArr[i].length; j++) {
+		 resArr[i][j] = 0;
+		}
+	}
+	//console.log(resArr)
+	for (let i = 0; i < resArr.length; i++) {
+	 	for (let j = 0; j < resArr[i].length; j++) {
+	 		if (matrix[i][j] == true) {
+	 			if(i - 1 >= 0){
+	 				if(j - 1 >= 0 && matrix[i-1][j-1] != true){
+	 					resArr[i-1][j-1]++;
+					}
+					if(j>= 0 && matrix[i-1][j] != true){
+						resArr[i-1][j]++;
+				   }
+					if(j + 1 < resArr[i].length && matrix[i-1][j+1] != true){
+						resArr[i-1][j+1]++;
+					}
+				}			
+				if(j - 1 >= 0 && matrix[i][j-1] != true){
+						resArr[i][j-1]++;
+				}			
+				
+				resArr[i][j]=1;
+
+				if(j + 1 < resArr[i].length && matrix[i][j+1] != true){
+					resArr[i][j+1]++;
+				}
+				if(i + 1 < resArr.length ){
+					if(j - 1 >= 0 && matrix[i+1][j-1] != true){
+						resArr[i+1][j-1]++;
+				   }
+			 		if(j>= 0 && matrix[i+1][j] != true){
+						resArr[i+1][j]++;
+				   }
+					if(j + 1 < resArr[i].length && matrix[i+1][j+1] != true){
+						resArr[i+1][j+1]++;
+					}
+				}	
+			}
+	 	}
+	}
+
+ return resArr;
 }
 
 module.exports = {
